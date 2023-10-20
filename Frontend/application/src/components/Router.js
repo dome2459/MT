8ac671abd1b153c7a7238394ddc052fd62e1e3f2
Sknulf <james.mueller@mailbox.org>
2023-10-20 11:29:07 +0200

    Router und Naigations componente

---
+++ Frontend/application/src/components/Router.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import RouterPicture from "../Pictures/Router.png"
+export default function Router()
+{
+ 
+ return(
+    <>
+    <div className="Router">    
+             <img src={RouterPicture} alt="Router"/> 
+    </div>
+      
+    </>
+    )
+}
