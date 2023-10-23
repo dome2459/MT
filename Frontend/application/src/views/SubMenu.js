3047de2c62a60871a41df4f6b88fb244aa7cf4e3
dominicgrafling <hedch11@gmail.com>
2023-10-23 09:56:51 +0200

    rest

---
+++ Frontend/application/src/views/SubMenu.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+const SidebarLink = styled(Link)`
+  display: flex;
+  color: #e1e9fc;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  list-style: none;
+  height: 60px;
+  text-decoration: none;
+  font-size: 18px;
+
+  &:hover {
+    background: #252831;
+    border-left: 4px solid #632ce4;
+    cursor: pointer;
+  }
+`;
+
+const SidebarLabel = styled.span`
+  margin-left: 0px;
+  margin-right: 0px;
+`;
+
+const DropdownLink = styled(Link)`
+  background: #414757;
+  height: 60px;
+  padding-left: 35px;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: #f5f5f5;
+  font-size: 18px;
+
+  &:hover {
+    background: #632ce4;
+    cursor: pointer;
+  }
+`;
+
+const Inputfield = styled(Link)`
+  color: black;
+  
+`;
+
+
+const SubMenu = ({ item }) => {
+    const [subnav, setSubnav] = useState(false);
+
+    const showSubnav = () => setSubnav(!subnav);
+
+    return (
+        <>
+            <SidebarLink to={item.path} onClick={item.subNav && showSubnav}>
+                <div>
+                    {item.icon}
+                    <SidebarLabel>{item.title}</SidebarLabel>
+                </div>
+                <div>
+                    {item.subNav && subnav
+                        ? item.iconOpened
+                        : item.subNav
+                            ? item.iconClosed
+                            : null}
+                </div>
+            </SidebarLink>
+            {subnav &&
+                item.subNav.map((item, index) => {
+                    return (
+                        <DropdownLink  key={index}>
+                            {/* {item.icon} */}
+                            <SidebarLabel>
+                                {item.title}
+                                <div>
+                                    <Inputfield>
+                                        <input />
+                                    </Inputfield>
+                                </div>
+                            </SidebarLabel>
+                            
+                        </DropdownLink>
+                    );
+                })}
+        </>
+    )
+
+}
+export default SubMenu;
