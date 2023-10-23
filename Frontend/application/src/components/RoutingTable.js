a46997950938703660b3253f16a2342ff084a27f
Sknulf <james.mueller@mailbox.org>
2023-10-23 12:42:59 +0200

    RoutingTablezu ui Hinzugefügt

--- Frontend/application/src/components/RoutingTable.js
+++ Frontend/application/src/components/RoutingTable.js
@@ -1,26 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+export default class RoutingTable extends Component {
+  render() {
+    // Hier erstellen Sie Ihr Objekt
+    const RoutingTable = {
+      routingTableID: "",
+      desrtination: "",
+      gateway: "",
+      networkMask: "",
+      interface: "",
+      metrik: "",
+    };
+
+    const rows = [
+      { Number: 4, Destination: '123.456.789.111', Gateway: 29, Interface: 'fa/02', Metric: 2, Networkmask: '255.255.255.192' },
+      { Number: 4, Destination: '123.456.789.111', Gateway: 29, Interface: 'fa/02', Metric: 2, Networkmask: '255.255.255.192' },
+      { Number: 4, Destination: '123.456.789.111', Gateway: 29, Interface: 'fa/02', Metric: 2, Networkmask: '255.255.255.192' },
+      { Number: 4, Destination: '123.456.789.111', Gateway: 29, Interface: 'fa/02', Metric: 2, Networkmask: '255.255.255.192' },
+      { Number: 4, Destination: '123.456.789.111', Gateway: 29, Interface: 'fa/02', Metric: 2, Networkmask: '255.255.255.192' },
+      { Number: 4, Destination: '123.456.789.111', Gateway: 29, Interface: 'fa/02', Metric: 2, Networkmask: '255.255.255.192' },
+      { Number: 4, Destination: '123.456.789.111', Gateway: 29, Interface: 'fa/02', Metric: 2, Networkmask: '255.255.255.192' },
+  ];
+
+    return (
+      <div>
+        {/* Verwenden Sie Ihr Objekt in JSX */}
+        {/* <p>Name: {RoutingTable.name}</p>
+        <p>Aktiv: {RoutingTable.isActiv}</p>
+        <p>:IP {RoutingTable.ip}</p> */}
 
 
-class RoutingTable extends Component {
-    render() {
-      // Hier erstellen Sie Ihr Objekt
-      const RoutingTable = {
-        routingTableID : '',
-        desrtination: '',
-        gateway: '',
-        networkMask: '',
-        interface : '',
-        metrik: ''
-      };
-  
-      return (
-        <div>
-          {/* Verwenden Sie Ihr Objekt in JSX */}
-          <p>Name: {RoutingTable.name}</p>
-          <p>Aktiv: {RoutingTable.isActiv}</p>
-          <p>:IP {RoutingTable.ip}</p>
-        </div>
-      );
-    }
+                  
+          <table  className="table" >
+              <tr>
+                <th className="tableHeader">Number</th>
+                <th className="tableHeader">Destination</th>
+                <th className="tableHeader">Gateway </th>
+                <th className="tableHeader">Interface </th>
+                <th className="tableHeader">Metric </th>
+                <th className="tableHeader">Networkmask </th>
+              </tr>
+            <tbody>
+             
+             {rows.map(({Number, Destination, Gateway, Interface, Metric, Networkmask}) => (
+              <tr>
+                <td className="tableRow">{Number}</td>
+                <td className="tableRow">{Destination}</td>
+                <td className="tableRow" isNumeric>{Gateway}</td>
+                <td className="tableRow" isNumeric>{Interface}</td>
+                <td className="tableRow" isNumeric>{Metric}</td>
+                <td className="tableRow" isNumeric>{Networkmask}</td>
+              </tr>
+             ))}
+            </tbody>
+          </table>
+      </div>
+    );
   }
-  
+}
--- Frontend/application/src/views/MainEnviroment.js
+++ Frontend/application/src/views/MainEnviroment.js
@@ -3,7 +3,7 @@ import { Flex, Tabs, TabList, TabPanels, Tab, TabPanel, Button, Menu, MenuButton
 import Moveable from "react-moveable";
 import DragArea from './DragArea';
 import Navbar from './Navbar';
-
+import RoutingTable from '../components/RoutingTable';
 
 
 export default class MainEnviroment extends Component {
@@ -17,12 +17,16 @@ export default class MainEnviroment extends Component {
 
         <Navbar />
 
+        <div className='test'>
         <Flex flex={3} margin={10} borderWidth={2} borderColor={'#ddd'}>
 
           <DragArea />
+          
 
         </Flex>
-
+        <RoutingTable/>
+        </div>
+          
         <Flex flex={1} bg='#eee'></Flex>
 
       </Flex>
