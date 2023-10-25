b0fed44e4b6678f5bd93b7ef1510ae4dbdb2a93e
SchaarOnMidHub <philippschaar01@gmail.com>
2023-10-25 12:21:27 +0200

    dragarea connections

--- Frontend/application/src/views/DragArea.js
+++ Frontend/application/src/views/DragArea.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
 import Draggable from 'react-draggable';
-import { Box, Text, Center } from "@chakra-ui/react";
+import { Box, Text, Center, Image } from "@chakra-ui/react";
+import {PathLine} from 'react-svg-pathline'
+import RouterSvg from './Router2.svg'
 
 export default function DragArea() {
 
@@ -10,7 +12,17 @@ export default function DragArea() {
         {name: 'Ali', x: 0, y: 0 },
         {name: 'Bob', x: 10, y: 10 },
         {name: 'CHAD', x: 20, y: 20 },
-        {name: 'Dic', x: 30, y: 30 }
+        {name: 'Dic', x: 30, y: 30 },
+        {name: 'Eric', x: 400, y: 100 },
+    ])
+    const [CableArray, setCableArray] = useState([
+
+      { connectionId: '1', routerA: 1, routerB : 2, ospf: '', rip : ''},
+      { connectionId: '2', routerA: 2, routerB : 4, ospf: '', rip : ''},
+      { connectionId: '3', routerA: 0, routerB : 3, ospf: '', rip : ''},
+      { connectionId: '4', routerA: 3, routerB : 2, ospf: '', rip : ''},
+      { connectionId: '5', routerA: 0, routerB : 4, ospf: '', rip : ''}
+
     ])
 
     const parentWidth = 1800;
@@ -29,15 +41,27 @@ export default function DragArea() {
 
         setRouterArray(RouterArr);
       };
+
+
     const RouterObj = (item, index) => {
         return(
             <Draggable bounds={{ left: 0, top: 0, right: maxX, bottom: maxY }} position={{x: item.x, y:item.y}} onDrag={handleDrag(index)} 
                     style={theme}>
-            <Center width={100} className="draggable"  borderWidth={1} borderColor='#000'  position="absolute"
 
-            cursor="grab" w="100px" h="100px" bg="skyblue" borderRadius={5}
+                      
+            <Center flexDirection={'column'} width={100} className="draggable" //borderWidth={1} borderColor='#000' 
+            position="absolute" cursor="grab" w="100px" h="100px"
 
-            >{item.name}</Center>
+            >
+
+          <Image src={RouterSvg} height={100} width={100} draggable={false} onmousedown={false}/>
+            
+            <Text marginBottom={-6} backgroundColor='#fff' borderRadius='3' paddingX={2} >{item.name}</Text>
+
+            </Center>
+
+
+
             </Draggable>
         )
     }
@@ -50,6 +74,22 @@ export default function DragArea() {
                      return RouterObj(item, i)
 
         })}
+      <svg style={{width: '100%', height: '100%'}}>
+
+        {CableArray.map((item, i) => {     
+                     
+            return(
+              <PathLine 
+              points={[{x:RouterArray[item.routerA].x + 50, y:RouterArray[item.routerA].y + 50}, {x:RouterArray[item.routerB].x + 50, y: RouterArray[item.routerB].y + 50}]} 
+              stroke="black" 
+              strokeWidth="3"
+              fill="none"
+              r={10}
+              /> 
+            )   
+        })}
+      </svg>
+
     </Box>
   );
 }
