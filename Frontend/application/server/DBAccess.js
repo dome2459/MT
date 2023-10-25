110469f2b775c98fe2ad13f3a213a4a4c35ec3dd
schoenni2 <nico.schoenfeld2004@web.de>
2023-10-25 11:55:36 +0200

    db

--- Frontend/application/server/DBAccess.js
+++ Frontend/application/server/DBAccess.js
@@ -1,6 +1,37 @@
-var express = require('express');
-var app = express();
-var mysql = require('mysql');
-var bodyParser = require('body-parser');
+import {useState} from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App(){
+
+    const [Name, setName] = useState('')
+    const [ConnectionId, setConnection] = useState('')
+
+    const submitHanlder = e => {
+        axios.post('http://localhost:3005/signup', {Name: Name, ConnectionId: ConnectionId})
+        .then((data) => {
+            console.log(data)
+            setName('')
+            setConnection('')
+        })
+    }
+
+    return(
+        <div>
+            <form onSubmit={submitHandler}>
+                <h3> SignUp</h3>
+                <label htmlFor='name'>Name</label>
+                <input id='name' type='text' value={Name} onChange={(e) => setName(e.target.value)}/>
+                <label htmlFor='connectionId'>ConnectionId</label>
+                <input id='connectionId' type='integer' value={ConnectionId} onChange={(e) => setConnection(e.target.value)}/>
+                <div>
+                <button type='button'>Cancel</button>
+                <button type='submit'>Submit</button>
+                </div>
+            </form>
+        </div>
+    );
+}
+export default App;
 
  
--- Frontend/application/server/index.js
+++ Frontend/application/server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
 const cors = require('cors');
-const mysql = require('mysql');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -16,19 +16,20 @@ var db = mysql.createPool({
   database        : 'mt'
 });
 
-app.get('/', (req,res) => {
-  db.query("INSERT INTO Router (username, password) VALUES ('Testing','123')",(err,result)=>{
+app.post('/signup', (req,res) => {
+  const Name = req.body.name;
+  const ConnectionId = req.body.ConnectionId;
+  db.query("INSERT INTO Router (Name, ConnectionId) VALUES (?, ?)",[Name, ConnectionId], (err,result) =>{
     if(err){
     console.log(err)
     }else{
-    console.log(result)
+    res.send({Name: Name})
     }
   })
   res.send('Test')
 })
 
-app.listen(3000,() => {
-console.log('server listening on port 3000');
-})
 
-//https://www.youtube.com/watch?v=syA6vUaZtOM
+app.listen(3005,() => {
+console.log('server listening on port 3005');
+})
