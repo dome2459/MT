564ccfcb9ec2617eb0debfda918e29026a5cd08c
schoenni2 <nico.schoenfeld2004@web.de>
2023-10-25 10:12:27 +0200

    Revert "Merge branch 'main' of https://github.com/dome2459/MT"
    This reverts commit e0a3813e7e63ce4379a3cd1702b4493556064dd3, reversing
    changes made to 0bf8b587125efee6842476f2cfe53314f6b1e9b1.

--- Frontend/application/src/views/MainEnviroment.js
+++ Frontend/application/src/views/MainEnviroment.js
@@ -3,7 +3,7 @@ import { Flex, Tabs, TabList, TabPanels, Tab, TabPanel, Button, Menu, MenuButton
 import DragArea from './DragArea';
 
 import Navbar from './Navbar';
-import RoutingTable from '../components/RoutingTable';
+
 
 
 export default class MainEnviroment extends Component {
@@ -17,17 +17,13 @@ export default class MainEnviroment extends Component {
 
         <Navbar />
 
-        <div className='test'>
         <Flex flex={3} margin={10} borderWidth={2} borderColor={'#ddd'}>
 
           <DragArea />
-          
 
 
         </Flex>
-        <RoutingTable/>
-        </div>
-          
+
         <Flex flex={1} bg='#eee'></Flex>
 
       </Flex>
