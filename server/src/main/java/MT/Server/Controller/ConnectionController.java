790b39c2f68f2bf1404ac3936c44d0a6e66857d7
schoenni2 <nico.schoenfeld2004@web.de>
2023-12-06 10:09:42 +0100

    Controller

--- server/src/main/java/MT/Server/Controller/ConnectionController.java
+++ server/src/main/java/MT/Server/Controller/ConnectionController.java
@@ -1,4 +1,66 @@
 package MT.Server.Controller;
 
+import MT.Server.Repos.connectionRepo;
+import MT.Server.ResourceNotFoundException;
+import MT.Server.Tables.Connection;
+import MT.Server.Tables.RoutingTable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin(origins = "https://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
 public class ConnectionController {
+
+
+  @Autowired
+  private connectionRepo connectionRepo;
+
+  @GetMapping("/connection")
+  public List<Connection> getAllConnection() {
+    return connectionRepo.findAll();
+  }
+
+  @PostMapping("/connection")
+  public Connection createConnection(@RequestBody Connection connection){
+    return connectionRepo.save(connection);
+  }
+
+  @GetMapping("/connection/{id}")
+  public ResponseEntity<Connection> getConnectionID(@PathVariable Long id){
+    Connection connection = connectionRepo.findById(id).orElseThrow( () -> new ResourceNotFoundException("routingTable mit Id: "+ id+" existiert nicht"));
+    return ResponseEntity.ok(connection);
+  }
+
+  @PutMapping("/connection/{id}")
+  public ResponseEntity<Connection> updateConnection(@PathVariable Long id, @RequestBody Connection connectionDetails){
+    Connection connection = connectionRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("routingTable mit Id: "+ id+" existiert nicht"));
+
+    connection.setConnectionId(connectionDetails.getConnectionId());
+    connection.setIp(connectionDetails.getIp());
+    connection.setOSPF(connectionDetails.getOSPF());
+    connection.setRIP(connectionDetails.getRIP());
+    connection.setRouterA(connectionDetails.getRouterA());
+    connection.setRouterB(connectionDetails.getRouterB());
+    connection.setRouterAInterface(connectionDetails.getRouterAInterface());
+    connection.setRouterBInterface(connectionDetails.getRouterBInterface());
+
+    Connection updatedConnection = connectionRepo.save(connection);
+    return ResponseEntity.ok(updatedConnection);
+  }
+
+  @DeleteMapping("/connection/{id}")
+  public ResponseEntity<Map<String, Boolean>> deleteConnection(@PathVariable Long id){
+    Connection connection = connectionRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("connection mit Id: "+ id+" existiert nicht"));
+
+    connectionRepo.delete(connection);
+    Map<String, Boolean> response = new HashMap<>();
+    response.put("deleted", Boolean.TRUE);
+    return  ResponseEntity.ok(response);
+  }
 }
--- server/src/main/java/MT/Server/Controller/SessionController.java
+++ server/src/main/java/MT/Server/Controller/SessionController.java
@@ -1,4 +1,61 @@
 package MT.Server.Controller;
 
+import MT.Server.Repos.sessionRepo;
+import MT.Server.ResourceNotFoundException;
+import MT.Server.Tables.Connection;
+import MT.Server.Tables.RoutingTable;
+import MT.Server.Tables.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin(origins = "https://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
 public class SessionController {
+
+
+  @Autowired
+  private sessionRepo sessionRepo;
+
+  @GetMapping("/session")
+  public List<Session> getAllSession() {
+    return sessionRepo.findAll();
+  }
+
+  @PostMapping("/session")
+  public Session createSession(@RequestBody Session session){
+    return sessionRepo.save(session);
+  }
+
+  @GetMapping("/session/{id}")
+  public ResponseEntity<Session> getSessionID(@PathVariable Long id){
+    Session session = sessionRepo.findById(id).orElseThrow( () -> new ResourceNotFoundException("session mit Id: "+ id+" existiert nicht"));
+    return ResponseEntity.ok(session);
+  }
+
+  @PutMapping("/session/{id}")
+  public ResponseEntity<Session> updateConnection(@PathVariable Long id, @RequestBody Session sessionTableDetails){
+    Session session = sessionRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("session mit Id: "+ id+" existiert nicht"));
+
+    session.setSessionId(sessionTableDetails.getSessionId());
+
+    Session updatedSession = sessionRepo.save(session);
+    return ResponseEntity.ok(updatedSession);
+  }
+
+  @DeleteMapping("/session/{id}")
+  public ResponseEntity<Map<String, Boolean>> deleteSession(@PathVariable Long id){
+    Session session = sessionRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("session mit Id: "+ id+" existiert nicht"));
+
+    sessionRepo.delete(session);
+    Map<String, Boolean> response = new HashMap<>();
+    response.put("deleted", Boolean.TRUE);
+    return  ResponseEntity.ok(response);
+  }
+
 }
