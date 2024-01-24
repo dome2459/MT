4d2de29b57c9ea12a75d9eb35d12d80ab4ee99ff
dominicgrafling <hedch11@gmail.com>
2024-01-24 09:50:14 +0100

    TimerController benötigt die Connection vom Frontend

--- server/src/main/java/MT/Server/Controller/TimerController.java
+++ server/src/main/java/MT/Server/Controller/TimerController.java
@@ -4,12 +4,16 @@ import MT.Server.Repos.connectionRepo;
 import MT.Server.Repos.routerRepo;
 import MT.Server.Repos.sessionRepo;
 import MT.Server.Repos.userRepo;
+import MT.Server.ResourceNotFoundException;
+import MT.Server.Tables.Connection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin(origins = "https://localhost:3000")
 @RestController
@@ -28,10 +32,11 @@ public class TimerController {
     private MT.Server.Repos.connectionRepo connectionRepo;
 
     @PostMapping("/start")
-    public ResponseEntity<String> startTimer() {
+    public ResponseEntity<Connection> startTimer(Connection connectionFromFrontend) {
+        Connection connection = connectionRepo.findById(connectionFromFrontend.getConnectionId()).orElseThrow(
+                () -> new ResourceNotFoundException("routingTable mit Id: "+ connectionFromFrontend.getConnectionId()+" existiert nicht"));
 
-
-        return ResponseEntity.ok("Timer gestartet");
+        return ResponseEntity.ok(connection);
     }
 
     @PostMapping("/stop")
