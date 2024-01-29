73a5f0a9b93689eeee73a58e74e9e2577bd3c5e8
SchaarOnMidHub <philippschaar01@gmail.com>
2024-01-29 12:02:17 +0100

    prev import fix

--- Frontend/application/src/components/MainController.js
+++ Frontend/application/src/components/MainController.js
@@ -2,9 +2,9 @@
         import React, { Component } from 'react';
         import { Flex, Tabs, TabList, TabPanels, Tab, TabPanel,Button } from '@chakra-ui/react'
         import MainEnviroment from '../views/MainEnviroment';
-        import RouterController from './components/RouterController';
-        import RoutingTableController from './components/RoutingTableController';
-        import ConnectionController from './components/ConnectionController';
+        import RouterController from './RouterController';
+        import RoutingTableController from './RoutingTableController';
+        import ConnectionController from './ConnectionController';
         import GlobalContext from './InitStateContext';
         
         
